08ec5a4174bb39d231aa5d6f74e2ba78f607f8a9
ardiprasetiyo <ardiprasetiyo@students.smkn3bandung.sch.id>
2019-12-25 15:03:16 +0700

    Adding Somechanges

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 // FORCE HTTPS (HEROKU)
 app.use(function(req, res, next) {
@@ -19,6 +20,7 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : false}))
+app.use(helmet())
 
 
 // ROUTING TABLE
@@ -30,7 +32,7 @@ app.get('/api/chat', function(req, res){
     {'fullname' : 'Si C', 'message' : 'Hello World'},
     {'fullname' : 'Si D', 'message' : 'Hello World'}]
 
-    res.send(dummy).statusCode(200)
+    res.send({'status_code' : 200, 'result' : dummy}).statusCode(200)
 })
 
 
